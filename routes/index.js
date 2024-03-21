ce69e82f5103a80893905e5758c23dba31240674
dchahla <dchahla@gmail.com>
2024-03-20 22:33:36 -0500

    getting up and procrastinating

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
-const router = require("express").Router();
+const router = require('express').Router()
 
-const register = require("./register");
-const login = require("./login");
-const logout = require("./logout");
-const profile = require("./profile");
-const commitments = require("./commitments");
+const register = require('./register')
+const login = require('./login')
+const logout = require('./logout')
+const profile = require('./profile')
+const commitments = require('./commitments')
 
 // endpoints
 
-router.get("/", (_req, res) => {
-  res.render("index");
-});
+router.get('/', (_req, res) => {
+  res.render('index')
+})
 
 // other routes
 
-router.use(register);
-router.use(login);
-router.use(logout);
-router.use(profile);
-router.use(commitments);
+router.use(register)
+router.use(login)
+router.use(logout)
+router.use(profile)
+router.use(commitments)
 
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,111 +1,111 @@
-require("dotenv").config();
-require("express-async-errors");
-const fs = require("node:fs");
-const path = require("node:path");
-const express = require("express");
-const logger = require("morgan");
-const http = require("http");
-const https = require("https");
-const { engine } = require("express-handlebars");
-const cookieSession = require("cookie-session");
-const cookieParser = require("cookie-parser");
+require('dotenv').config()
+require('express-async-errors')
+const fs = require('node:fs')
+const path = require('node:path')
+const express = require('express')
+const logger = require('morgan')
+const http = require('http')
+const https = require('https')
+const { engine } = require('express-handlebars')
+const cookieSession = require('cookie-session')
+const cookieParser = require('cookie-parser')
 
-const package = require("./package.json");
-const { auth, redirectToLogin } = require("./utils/auth");
-const routes = require("./routes");
+const package = require('./package.json')
+const { auth, redirectToLogin } = require('./utils/auth')
+const routes = require('./routes')
 
-const { COOKIE_SECRET } = process.env;
+const { COOKIE_SECRET } = process.env
 
-const app = express();
+const app = express()
 
 // session and authentication
 
 app.use(
   cookieSession({
     secret: COOKIE_SECRET,
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
   })
-);
+)
 
-app.use(auth());
+app.use(auth())
 
 // other global middlewares
 
-app.use(cookieParser(COOKIE_SECRET));
+app.use(cookieParser(COOKIE_SECRET))
 
-app.use(logger("dev"));
+app.use(logger('dev'))
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars')
 app.engine(
-  "handlebars",
+  'handlebars',
   engine({
-    defaultLayout: "main",
+    defaultLayout: 'main'
   })
-);
+)
 
 // make user data available in all views
 app.use((req, res, next) => {
-  res.locals.user = req.user;
+  res.locals.user = req.user
 
-  next();
-});
+  next()
+})
 
 // routes
-app.use(routes);
+app.use(routes)
 
 // catch 404 and forward to error handler
 app.use(function (_req, _res, next) {
-  const err = new Error("Not Found");
-  err.status = 404;
+  const err = new Error('Not Found')
+  err.status = 404
 
-  next(err);
-});
+  next(err)
+})
 
 // error handler
 app.use(function (err, req, res, _next) {
-  const { message, status = 500 } = err;
-  const description = status >= 500 ? "Something unexpected happened" : message;
-  const details = process.env.NODE_ENV !== "production" ? err.stack : "";
+  const { message, status = 500 } = err
+  const description = status >= 500 ? 'Something unexpected happened' : message
+  const details = process.env.NODE_ENV !== 'production' ? err.stack : ''
 
   if (status === 401) {
-    console.log("Redirecting UNAUTHORIZED to login page");
+    console.log('Redirecting UNAUTHORIZED to login page')
 
-    return redirectToLogin(req, res);
+    return redirectToLogin(req, res)
   }
 
-  res.status(status);
+  res.status(status)
 
   if (status === 404) {
-    return res.render("not_found");
+    return res.render('not_found')
   }
   if (status >= 500) {
-    console.error("ERROR:", err);
+    console.error('ERROR:', err)
   }
 
-  res.render("error", {
+  res.render('error', {
     status,
     description,
-    details,
-  });
-});
+    details
+  })
+})
 
 // start the server
 const server =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === 'production'
     ? http.createServer(app)
     : https.createServer(
         {
-          key: fs.readFileSync(path.resolve("./cert/dev.key")),
-          cert: fs.readFileSync(path.resolve("./cert/dev.crt")),
+          key: fs.readFileSync(path.resolve('./cert/dev.key')),
+          cert: fs.readFileSync(path.resolve('./cert/dev.crt'))
         },
         app
-      );
+      )
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 server.listen(port, () => {
   console.log(
     `${package.description} (v${package.version}), listening on port ${port}`
-  );
-});
+  )
+})
